cacd7b045ab66b2dc3c1db3ea01e2a303adbcbd2
yikesoftware <1532494123@qq.com>
2021-11-02 16:52:57 +0800

    Add 第二次上机作业

---
+++ Reports/1/RE_LAB2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <windows.h>
+#include <cstdlib>
+#include <cstring>
+
+DWORD WINAPI showMsg(LPVOID targetModuleName) {
+    if (!targetModuleName) {
+        MessageBoxA(NULL, "Please set 'targetModuleName'!", "ERROR", MB_YESNO);
+        return ~0;
+    }
+
+    char nameBuf[MAX_PATH];
+    char msgBuf[MAX_PATH + 0x100];
+
+    GetModuleFileNameA(GetModuleHandleA((LPCSTR)targetModuleName), nameBuf, MAX_PATH);
+    FARPROC GetThreadIdStrAddr = GetProcAddress(GetModuleHandleA("kernel32"), "GetCurrentThreadId");
+    DWORD threadId = GetThreadIdStrAddr();
+
+    snprintf(msgBuf, MAX_PATH + 0xFF, "%s; id=%u", nameBuf, threadId);
+
+    MessageBoxA(NULL, msgBuf, "Message", MB_YESNO);
+    return 0;
+}
+
+char dllName[] = "kernel32.dll";
+
+int main()
+{
+    HANDLE msgThreadHandle = CreateThread(NULL, 0, showMsg, dllName, 0, NULL);
+    WaitForSingleObject(msgThreadHandle, INFINITE);
+    return 0;
+}
